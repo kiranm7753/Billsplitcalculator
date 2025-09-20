71ea7504b0695c1178196c385924e787ea1b37c0
Sasi Kiran Mullapudi <123757564+kiranm7753@users.noreply.github.com>
2025-09-20 15:26:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Fixed roommate list and groups
-  const roommates = ["DEEKSHITH", "NAGI", "NANDITH", "PAVAN", "RAVI", "SASI"];
+  const roommates = ["DEEKSHITH", "NAGI", "NANDITH", "PAVAN", "RAVI", "SASI", "SUNDER"];
   const nonVegGroup = ["PAVAN", "SASI", "RAVI", "NANDITH"];
   const eggGroup = ["NAGI", "PAVAN", "SASI", "RAVI", "NANDITH"];
 
@@ -284,5 +284,6 @@ document.addEventListener("DOMContentLoaded", function () {
     doc.save("Roommate_Splitter_Summary.pdf");
   };
 });
+
 
 
